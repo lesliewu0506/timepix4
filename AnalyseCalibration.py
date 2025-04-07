ea01854a8ecf14e1e7d9b78ea006cdc0d2f0e5d5
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-04-07 15:56:35 +0200

    Displayed multiple single pixels ToT Charge Plots in one plot

--- AnalyseCalibration.py
+++ AnalyseCalibration.py
@@ -73,25 +73,42 @@ def PlotTotChargeSingle(filepath, TestCol, TestRow):
             df_plot = pd.concat([df_plot, new_row], ignore_index=True)
 
     # Plotting
-    popt, pcov = curve_fit(FitFunction, df_plot["Charge"] / 1000, df_plot["meanTot"],
-                           p0=[0.001, 0, 1, 100])  # p0 is the initial guess for [a, b, c, t]
+    # popt, pcov = curve_fit(FitFunction, df_plot["Charge"] / 1000, df_plot["meanTot"],
+    #                        p0=[0.001, 0, 1, 100])  # p0 is the initial guess for [a, b, c, t]
+    
+    # Q_fit = np.linspace(df_plot["Charge"].min() / 1000, df_plot["Charge"].max() / 1000, 2)
+    # Tot_fit = FitFunction(Q_fit, *popt)
 
-    # Generate fitted curve
-    Q_fit = np.linspace(df_plot["Charge"].min() / 1000, df_plot["Charge"].max() / 1000, 2)
-    Tot_fit = FitFunction(Q_fit, *popt)
+    return df_plot
 
-    # Plot the data points and fitted curve
-    plt.scatter(df_plot["Charge"] / 1000, df_plot["meanTot"], color="blue", label="Mean ToT")
-    plt.plot(Q_fit, Tot_fit, color="red", label="Fit")
-    plt.xlabel("Charge [ke]")
-    plt.ylabel("ToT [25ns]")
-    plt.legend()
-    plt.title("ToT vs Charge with Fit N116")
-    # plt.savefig("Tot_vs_Charge_N10.png", dpi = 600)
+def VisualizeMultipleToTChargeSingle(filepath1, filepath2):
+    figs, axs = plt.subplots(4, 4, figsize = (20, 20), sharex = False)
+    j = 0
+    for i, pixel in enumerate(range(50, 450, 50)):
+        k = i
+        if i > 3:
+            j = 1
+            k = i - 4
+        
+        df1= PlotTotChargeSingle(filepath1, pixel, pixel)
+        df2= PlotTotChargeSingle(filepath2, pixel, pixel)
+        
+        axs[j, k].scatter(df1["Charge"] / 1000, df1["meanTot"], color = "blue", label = "Mean ToT N10")
+        # axs[j, k].plot(Q_fit1, Tot_fit1, color = "red", label = "Fit N10")
+        axs[j, k].set_xlabel("Charge [ke]")
+        axs[j, k].set_ylabel("ToT [25ns]")
+        axs[j, k].legend()
+        axs[j, k].set_title(f"ToT vs Charge with Fit N10 ({pixel} x {pixel})")
+        axs[j + 2, k].scatter(df2["Charge"] / 1000, df2["meanTot"], color = "blue", label = "Mean ToT N116")
+        # axs[j + 2, k].plot(Q_fit2, Tot_fit2, color = "red", label = "Fit N116")
+        axs[j + 2, k].set_xlabel("Charge [ke]")
+        axs[j + 2, k].set_ylabel("ToT [25ns]")
+        axs[j + 2, k].legend()
+        axs[j + 2, k].set_title(f"ToT vs Charge with Fit N116 ({pixel} x {pixel})")
+
+    plt.savefig("Tot_vs_Charge.png", dpi = 600)
     plt.show()
-
-    # Print the fitted parameters
-    print("Fitted parameters: a=%.6f, b=%.6f, c=%.6f, t=%.6f" % tuple(popt))
+    # print("Fitted parameters: a=%.6f, b=%.6f, c=%.6f, t=%.6f" % tuple(popt))
 
 def STDHistogram(filepath1, filepath2):
     plt.figure(figsize=(8, 6))
@@ -110,6 +127,5 @@ def STDHistogram(filepath1, filepath2):
     plt.show()
 
 if __name__ == "__main__":
-    for i in range(100, 450, 50):
-        PlotTotChargeSingle("fitData_N116.csv", i, i)
     # STDHistogram("fitData_N10.csv", "fitData_N116.csv")
+    VisualizeMultipleToTChargeSingle("fitData_N10.csv", "fitData_N116.csv")
