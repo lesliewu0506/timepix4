c1c11845534f2a6f6d7f44da38d88b017695f151
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-04-09 21:29:33 +0200

    Updated RootToCSV Script

--- ManualCalibration.py
+++ ManualCalibration.py
@@ -2,56 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-THRESHOLDTOT = 150
-
-def LoadCSV(filepath):
-    df = pd.read_csv(filepath, usecols=["nhits", "col", "row", "tot"])
-
-def FindHighestToT(df, target_col, target_row):
-    # Filter for pixel
-    pixel_df = df[(df['col'] == target_col) & (df['row'] == target_row)]
-
-    if pixel_df.empty:
-        return None
-
-    counts, bin_edges = np.histogram(pixel_df['tot'], bins=301)
-
-    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
-
-    # Can be higher than 100
-    mask = bin_centers > THRESHOLDTOT
-    filtered_counts = counts[mask]
-    filtered_bins = bin_centers[mask]
-
-    if len(filtered_bins) == 0:
-        return None
-
-    # Find Dominant Bin
-    max_index = np.argmax(filtered_counts)
-    dominant_bin_value = filtered_bins[max_index]
-
-    return dominant_bin_value
-
-def GetCalibrationFactors(df: pd.DataFrame):
-    groups = df.groupby("pixel")
-
-    global CorrectionFactors
-
-    CorrectionFactors = {
-        pixel: 16.5 / FindHighestToT(group, group["col"].iloc[0], group["row"].iloc[0])
-        for pixel, group in groups
-        if FindHighestToT(group, group["col"].iloc[0], group["row"].iloc[0]) is not None
-    }
-
-def CalculateCharge(df:pd.DataFrame, save: bool = False):
-    df["correction"] = df["pixel"].apply(lambda pixel: CorrectionFactors.get(pixel, np.nan))
-    df["charge"] = df["tot"] * df["correction"]
-    
-    if save:
-        df.to_csv("N116-filtered.csv", index = False)
-    
-    return df
-
 def PlotHistogram(df:pd.DataFrame = None, filepath:str = None):
     if filepath is not None:
         df = pd.read_csv(filepath)
@@ -81,43 +31,41 @@ def PlotHistogram(df:pd.DataFrame = None, filepath:str = None):
     plt.savefig("N116_Charge_Distribution_Manual.png", dpi = 600)
     plt.show()
 
-def FilterNormalData(filepath):
-    df = pd.read_csv(filepath, usecols = ["nhits", "charge"])
-    df_filtered = df[df["nhits"] == 1].copy()
-
-    df_filtered["charge"] = df_filtered["charge"].str.strip("[]").astype(float) / 1000
-
-    return df_filtered
-
-def PlotTwoToT(filepath1, filepath2):
-    df_auto = FilterNormalData(filepath1)
-    df_manual = pd.read_csv(filepath2)
-    df_manual.loc[df_manual["charge"] > 25, "charge"] = np.nan
+def PlotCompare(filepath_raw, filepath_manual):
+    df_auto = pd.read_csv(f"Raw Charge Data/{filepath_raw}-Raw-Charge.csv")
     data_auto = df_auto["charge"].dropna()
+
+    df_manual = pd.read_csv(f"Calibration Data/{filepath_manual}-Filtered.csv")
+    
+    df_manual.loc[df_manual["charge"] > 25, "charge"] = np.nan
     data_manual = df_manual["charge"].dropna()
     
-    # Plot beide in één histogram
-    plt.hist(data_auto, bins=1600, alpha=0.6, label="Auto Calibration", color="blue", density=True)
-    plt.hist(data_manual, bins=80, alpha=0.6, label="Manual Calibration", color="orange", density=True)
+    plt.hist(data_auto, bins=1600, alpha=0.6, label="Test Pulse Calibration", color="blue", density=True)
+    plt.hist(data_manual, bins=160, alpha=0.6, label="Manual Calibration", color="orange", density=True)
 
-    plt.xlabel("Charge (ke)")
+    plt.xlabel("Charge [ke]")
     plt.ylabel("Counts")
     plt.xlim(0, 20)
+
+    vlines = [
+        {"x": 2.225, "color": "red",    "label": "8.01 keV"},
+        {"x": 3.861, "color": "green",  "label": "13.9 keV"},
+        {"x": 4.917, "color": "blue",   "label": "17.7 keV"},
+        {"x": 5.75,  "color": "magenta","label": "20.7 keV"},
+        {"x": 7.306, "color": "cyan",   "label": "26.3 keV"},
+        {"x": 16.5,  "color": "orange", "label": "59.5 keV"},
+    ]
+    for line in vlines:
+        plt.axvline(x=line["x"], color=line["color"], linestyle='--', label=line["label"])
+    
     plt.title("Charge Distribution N10")
     plt.grid(False)
     plt.legend()
     plt.savefig("N10_Charge_Distribution_ManualvsAuto.png", dpi = 600)
     plt.show()
 
-def main(filepath):
-    df = LoadCSV(filepath)
-    GetCalibrationFactors(df)
-    df = CalculateCharge(df, save = True)
-    PlotHistogram(df = df)
-
 if __name__ == "__main__":
     # filepath = "N116-250403-150114-filtered.csv" 
     # filepath = "N116-250408-123554.csv"
-    # main(filepath)
-    PlotHistogram(filepath = "Calibration Data/N116-filtered.csv")
-    # PlotTwoToT("N10-250404-143700_normal.csv", "N10-filtered.csv")
+
+    PlotCompare("N10-250409-113850", "N10-250409-113850")
--- RootToCSV.py
+++ RootToCSV.py
@@ -1,5 +1,6 @@
 import uproot
 import pandas as pd
+import numpy as np
 
 def ConvertFitData(root_file_path, csv_file_path):
     file = uproot.open(root_file_path)
@@ -20,23 +21,84 @@ def ConvertFitData(root_file_path, csv_file_path):
     # Save to CSV
     df_data.to_csv(csv_file_path, index=False)
 
-def ConvertManualCalibrationData(root_file_path, csv_file_path):
-    file = uproot.open(root_file_path)
-    tree_name = "clusterTree"
+# =========================Calculating factors===================
+THRESHOLDTOT = 150
 
-    # Access the tree
-    tree = file[tree_name]
+def FindHighestToT(df, target_col, target_row):
+    # Filter for pixel
+    pixel_df = df[(df['col'] == target_col) & (df['row'] == target_row)]
 
-    # Convert the TTree data into a Pandas DataFrame
-    # The `arrays()` function reads the specified branches; specifying library="pd" returns a DataFrame.
-    df = tree.arrays(["nhits", "col", "row", "tot"], library="pd")
+    if pixel_df.empty:
+        return None
 
-    # # Write the DataFrame to a CSV file
-    df.to_csv(csv_file_path, index=False)
+    counts, bin_edges = np.histogram(pixel_df['tot'], bins=301)
+
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+
+    # Can be higher than 100
+    mask = bin_centers > THRESHOLDTOT
+    filtered_counts = counts[mask]
+    filtered_bins = bin_centers[mask]
+
+    if len(filtered_bins) == 0:
+        return None
+
+    # Find Dominant Bin
+    max_index = np.argmax(filtered_counts)
+    dominant_bin_value = filtered_bins[max_index]
+
+    return dominant_bin_value
+
+def GetCalibrationFactors(df: pd.DataFrame):
+    groups = df.groupby("pixel")
+
+    global CorrectionFactors
+
+    CorrectionFactors = {
+        pixel: 16.5 / FindHighestToT(group, group["col"].iloc[0], group["row"].iloc[0])
+        for pixel, group in groups
+        if FindHighestToT(group, group["col"].iloc[0], group["row"].iloc[0]) is not None
+    }
+
+def CalculateCharge(df:pd.DataFrame):
+    GetCalibrationFactors(df)
+    df["correction"] = df["pixel"].apply(lambda pixel: CorrectionFactors.get(pixel, np.nan))
+    df["charge"] = df["tot"] * df["correction"]
+
+    return df
+
+# =========================Calculating factors===================
+
+def FilterAndUnwrap(df: pd.DataFrame) -> pd.DataFrame:
+    df_filtered = df[df["nhits"] == 1].copy()
+    df_filtered = df_filtered.map(lambda x: x[0] if isinstance(x, (list, tuple)) and len(x) == 1 else x)
+    df_filtered["Raw Charge"] = df_filtered["Raw Charge"].astype(float) / 1000
+    return df_filtered
+
+def TransformDataFrame(df_filtered: pd.DataFrame) -> pd.DataFrame:
+    df_filtered["pixel"] = list(zip(df_filtered["col"], df_filtered["row"]))
+    df_transformed = CalculateCharge(df_filtered)
+    return df_transformed.dropna(subset=["charge"])
+
+def ConvertClusterData(filepath: str) -> None:
+    filename = filepath.split(".")[0]
+    file = uproot.open(f"Data/Am-241 Runs/{filepath}")
+    tree = file["clusterTree"]
+    
+    arrays_data = tree.arrays(["col", "row", "tot", "nhits", "charge"], library="pd")
+    df_data = pd.DataFrame({
+        "col": arrays_data["col"].to_numpy(),
+        "row": arrays_data["row"].to_numpy(),
+        "tot": arrays_data["tot"].to_numpy(),
+        "nhits": arrays_data["nhits"].to_numpy(),
+        "Raw Charge": arrays_data["charge"].to_numpy()
+    })
+
+    df_filtered = FilterAndUnwrap(df_data)
+    df_transformed = TransformDataFrame(df_filtered)
+    df_transformed.to_csv(f"Data/Filtered Calibration Data/{filename}-Charge-Data.csv", index=False, columns = ["charge", "Raw Charge"])
 
 if __name__ == "__main__":
-    # Specify the ROOT file path
-    root_file_path = "Am-241 Runs/N116-250408-123554.root"
-    csv_file_path = "N116-250408-123554.csv"
 
-    ConvertManualCalibrationData(root_file_path, csv_file_path)
+    for root_file_path in ["N10-250409-113850.root"]:
+        ConvertClusterData(root_file_path)
