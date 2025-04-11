293d7ae68b344e516e7b56878b30f8b7fd4c1c71
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-04-11 14:32:08 +0200

    Updated comparison plots for multiple sensors

--- HeatmapFitParams.py
+++ HeatmapFitParams.py
@@ -5,6 +5,7 @@ import seaborn as sns
 def LoadCSV(filepath):
     df = pd.read_csv(filepath, delim_whitespace=True, header=None, skiprows=15)
     df.columns = ["Col", "Row", "p0", "p1", "p2", "p3", "p0_err", "p1_err", "p2_err", "p3_err", "red_chi2", "valid"]
+    df = df.drop(columns=["p0_err", "p1_err", "p2_err", "p3_err", "red_chi2", "valid"])
     return df
 
 def PlotChargeCalibrationsAll(filepath: str, parameters: list):
@@ -31,7 +32,36 @@ def PlotChargeCalibrationsAll(filepath: str, parameters: list):
     plt.savefig("All_Calibration_Heatmaps.png", dpi=600)
     plt.show()
 
+def PlotHeatMapMultiple(sensors: list[str]):
+    fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+    fig.suptitle("Heatmaps of p1 per pixel for different sensors", fontsize=18)
+    axes = axes.flatten()
+
+    for i, sensor in enumerate(sensors):
+        df = LoadCSV(f"Data/Charge Calibrations/{sensor}_charge.txt")
+        heatmap_data = df.pivot(index="Row", columns="Col", values="p1")
+        sns.heatmap(
+            heatmap_data,
+            cmap="viridis",
+            cbar_kws={"label": "p1"},
+            ax=axes[i],
+            square=True,
+            vmin = 0.65,
+            vmax = 2.5
+        )
+        axes[i].set_title(sensor)
+        axes[i].set_xticks([])
+        axes[i].set_yticks([])
+        axes[i].set_xlabel("")
+        axes[i].set_ylabel("")
+    
+    plt.tight_layout()
+    plt.savefig("All_Sensors_Charge_Heatmaps.png", dpi=600)
+    plt.show()
 if __name__ == "__main__":
-    filepath = "Charge Calibrations/N116_charge.txt"
-    parameters = ["p0", "p1", "p2", "p3"]
-    PlotChargeCalibrationsAll(filepath, parameters)
+    # filepath = "Charge Calibrations/N116_charge.txt"
+    # parameters = ["p0", "p1", "p2", "p3"]
+    # PlotChargeCalibrationsAll(filepath, parameters)
+
+    sensors = ["N10", "N112", "N113", "N116"]
+    PlotHeatMapMultiple(sensors)
