b38ad7d4d55651ceebebdf1da785c94ac1cab37b
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-04-15 13:19:08 +0200

    Created IV curve for N146

--- IVPlot.py
+++ IVPlot.py
@@ -1,32 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def CreateDataframe(filepath):
-    df = pd.read_csv(f"Voltage Scans/{filepath}.txt", sep=' ', header=None)
 
-    df.columns = ['Voltage', 'Current', 'Std']
-    df['Current'] = df['Current'] * (-10 ** 6)
-    
+def CreateDataframe(filepath):
+    df = pd.read_csv(f"Data/Voltage Scans/{filepath}.txt", sep=" ", header=None)
+
+    df.columns = ["Voltage", "Current", "Std"]
+    df["Current"] = df["Current"] * ((10**6))
+
     return df
 
+
 def PlotIV(filepath: str):
-    FileName = filepath.split('_')[0]
     df = CreateDataframe(filepath)
     # Plot Current vs Voltage
-    plt.figure()
-    plt.plot(df['Voltage'], df['Current'], marker='o', markersize = 5)
-    plt.xlabel('Voltage [V]')
-    plt.ylim(0, 2)
-    plt.ylabel('Current [$\mu$A]')
-    plt.gca().invert_xaxis()
-    plt.title(f'I-V Curve N10 with Tape')
+    plt.figure(figsize=(14,8))
+    plt.plot(df["Voltage"], df["Current"], marker="o", markersize=5)
+    plt.xlabel("Absolute Voltage [V]")
+    plt.xlim(0, 200)
+    plt.ylim(0, 10)
+    plt.ylabel("Leakage Current [$\mu$A]")
+    # plt.gca().invert_xaxis()
+    plt.title(f"IV-Curve N146")
     plt.tight_layout()
-    
-    plt.savefig(f'IV_curve_N10_with_Tape.png', dpi=600)
+
+    plt.savefig(f"IV_curve_N146.png", dpi=600)
     plt.show()
+
 
 if __name__ == "__main__":
     # files = [f"{pre}N10_voltage_scan.txt", f"{pre}N116_voltage_scan.txt"]
-    files = ["N10_voltage_scan_Tape"]
+    files = ["N146_Voltage_Scan"]
     for file in files:
         PlotIV(file)
