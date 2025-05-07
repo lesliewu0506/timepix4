91bfe9b8bec97adc48fde5dd0a2d0567821e8598
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2025-05-07 10:15:31 +0200

    Remove unused functions and clean up ConvertToT4Sector in 1RootToCSV.py

--- 1RootToCSV.py
+++ 1RootToCSV.py
@@ -2,93 +2,6 @@ import uproot, os, ast
 import pandas as pd
 import numpy as np
 
-# =========================Calculating factors===================
-THRESHOLDTOT = 150
-
-
-def FindHighestToT(df, target_col, target_row):
-    # Filter for pixel
-    pixel_df = df[(df["col"] == target_col) & (df["row"] == target_row)]
-
-    if pixel_df.empty:
-        return None
-    mask = pixel_df["tot"] > THRESHOLDTOT
-    filtered_tot = pixel_df.loc[mask, "tot"]
-    if filtered_tot.empty:
-        return None
-    return filtered_tot.mean()
-
-
-def GetCalibrationFactors(df: pd.DataFrame):
-    groups = df.groupby("pixel")
-
-    global CorrectionFactors
-
-    CorrectionFactors = {
-        pixel: 16.5 / FindHighestToT(group, group["col"].iloc[0], group["row"].iloc[0])
-        for pixel, group in groups
-        if FindHighestToT(group, group["col"].iloc[0], group["row"].iloc[0]) is not None
-    }
-
-
-def CalculateCharge(df: pd.DataFrame):
-    GetCalibrationFactors(df)
-    df["correction"] = df["pixel"].apply(
-        lambda pixel: CorrectionFactors.get(pixel, np.nan)
-    )
-    df["charge"] = df["tot"] * df["correction"]
-
-    return df
-
-
-# =========================Calculating factors===================
-
-
-def FilterAndUnwrap(df: pd.DataFrame) -> pd.DataFrame:
-    df_filtered = df[df["nhits"] == 1].copy()
-    df_filtered = df_filtered.map(
-        lambda x: x[0] if isinstance(x, (list, tuple)) and len(x) == 1 else x
-    )
-    df_filtered["Raw Charge"] = df_filtered["Raw Charge"].astype(float) / 1000
-    return df_filtered
-
-
-def TransformDataFrame(df_filtered: pd.DataFrame) -> pd.DataFrame:
-    df_filtered["pixel"] = list(zip(df_filtered["col"], df_filtered["row"]))
-    df_transformed = CalculateCharge(df_filtered)
-    return df_transformed.dropna(subset=["charge"])
-
-
-def ConvertClusterData(filepath: str) -> None:
-    filename = filepath.split(".")[0]
-    file = uproot.open(f"Data/Am-241 Runs/{filepath}")
-    tree = file["clusterTree"]
-
-    arrays_data = tree.arrays(["col", "row", "tot", "nhits", "charge"], library="pd")
-    df_data = pd.DataFrame(
-        {
-            "col": arrays_data["col"].to_numpy(),
-            "row": arrays_data["row"].to_numpy(),
-            "tot": arrays_data["tot"].to_numpy(),
-            "nhits": arrays_data["nhits"].to_numpy(),
-            "Raw Charge": arrays_data["charge"].to_numpy(),
-        }
-    )
-
-    df_filtered = FilterAndUnwrap(df_data)
-    df_filtered.to_csv(
-        f"Data/Filtered Calibration Data/{filename}-Charge-Data.csv",)
-    # df_transformed = TransformDataFrame(df_filtered)
-    # df_transformed["correction"] = df_transformed["pixel"].apply(
-    #     lambda pixel: CorrectionFactors.get(pixel, np.nan)
-    # )
-    # df_correction = pd.DataFrame(list(CorrectionFactors.items()), columns=["pixel", "correction"])
-
-    # df_correction.to_csv(f"Data/Filtered Calibration Data/{filename}-CorrectionFactors.csv", index=False)
-    # df_transformed.to_csv(
-    #     f"Data/Filtered Calibration Data/{filename}-Charge-Data.csv", index=False
-    # )
-
 
 def ConvertToT4Sector(filepath: str) -> None:
     filename = filepath.split(".")[0]
@@ -242,7 +155,12 @@ def SinglePixel(folder):
         row_list = [to_scalar(item) for item in arrays_data["row"]]
 
         df = pd.DataFrame(
-            {f"col {Charge}" : col_list, f"row {Charge}" : row_list, "tot": tot_list, "nhits": nhits_list}
+            {
+                f"col {Charge}": col_list,
+                f"row {Charge}": row_list,
+                "tot": tot_list,
+                "nhits": nhits_list,
+            }
         )
         df_filtered = df[df["nhits"] == 1]
         df_filtered = df_filtered.rename(columns={"tot": f"Charge {Charge}"})
@@ -261,18 +179,3 @@ if __name__ == "__main__":
         "N113-250408-100406.root",
         "N116-250403-150114.root",
     ]
-    # for filepath in filepaths:
-    # ConvertClusterData(filepath)
-    # ConvertToT4Sector(filepath)
-    for root_file_path in ["N116-250408-123554.root"]:
-    # root_file_path = "N10-250409-113326.root"
-        ConvertClusterData(root_file_path)
-    # ConvertToT4Sector("N116-250408-123554.root")
-    # ConvertToT4Sector("N116-250408-105332.root")
-    # Threshold("Data/Threshold Test Data/")
-    # FilterThreshold("Data/Threshold Test Data/FinalHits.csv")
-    # Threshold("Data/Threshold Test Data/N113/")
-    # ConvertToT4Sector("N112-250411-101613.root")
-    # ConvertFitData("Data/Test Pulse Data/N116-250417-093801.root", "Data/Test Pulse Data/fitData_N116_3.csv")
-    # Threshold("Data/Threshold Test Data/N116_2/")
-    # SinglePixel(f"Data/Single Pixel Data/Sectors/Multiple/BR/")
